5fef93dae27b7513622e7f6740d5f3cc2f9d0aab
Arjunsinh Jadeja <arjunsinh@outlook.com>
2017-06-15 05:22:57 +0530

    misc commit

--- getNumber.php
+++ getNumber.php
@@ -2,7 +2,7 @@
 $fp = fopen("requestQueue", "r+") or die("Unable to open file!");
 $queue = file_get_contents("requestQueue");
 $first_num = substr($queue, 0, 10);
-echo "Number:".$first_num;
+echo $first_num;
 file_put_contents("requestQueue", substr($queue, 10 + 1));
 fclose($fp);
 ?>
