51c40451cb270a066c40847eb978ac6ce94bcdc1
Arjunsinh Jadeja <arjunsinh@outlook.com>
2017-06-14 13:52:41 +0530

    test 2 read and delete first number

--- getNumber.php
+++ getNumber.php
@@ -1,7 +1,7 @@
 <?php
 echo "Base script working\n";
 $fp = fopen("requestQueue", "r") or die("Unable to open file!");
-$contents = file_get_contents($file));
+$contents = file_get_contents($file);
 $first_line = substr($contents, 0, 9);
 echo $first_line;
 file_put_contents($file, substr($contents, 9 + 1));
