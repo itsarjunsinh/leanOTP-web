7cb66cdbc6227df555dce87795a63159d739757c
Arjunsinh Jadeja <arjunsinh@outlook.com>
2017-06-14 14:25:31 +0530

    working read and delete first number

--- getNumber.php
+++ getNumber.php
@@ -1,15 +1,8 @@
 <?php
-echo "Base script working\n";
 $fp = fopen("requestQueue", "r+") or die("Unable to open file!");
 $queue = file_get_contents("requestQueue");
-$first_num = substr($queue, 0, 9);
-echo "\n1:".$queue;
-echo "\n2:".$first_num;
-$queue = file_get_contents("./requestQueue");
-$first_num = substr($queue, 0, 9);
-echo "\n21:".$queue;
-echo "\n22:".$first_num;
-echo "\n23:".fgets($fp);
-file_put_contents($fp, substr($queue, 9 + 1));
+$first_num = substr($queue, 0, 10);
+echo "Number:".$first_num;
+file_put_contents("requestQueue", substr($queue, 10 + 1));
 fclose($fp);
 ?>
