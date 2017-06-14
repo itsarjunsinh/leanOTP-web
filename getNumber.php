84207bd98b4f7cd7b18633cce28ba8099eb0ace3
Arjunsinh Jadeja <arjunsinh@outlook.com>
2017-06-14 13:39:38 +0530

    test 3 read first number

--- getNumber.php
+++ getNumber.php
@@ -1,6 +1,6 @@
 <?php
 echo "Base script working\n";
-$fp = fopen('requestQueue', 'r') or die("Unable to open file!");
+$fp = fopen("requestQueue", "r") or die("Unable to open file!");
 echo $fgets($fp);
 fclose($fp);
 ?>
