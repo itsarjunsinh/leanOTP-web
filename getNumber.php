9edc924c6a710c37f13ba581401015e6ea0ac76b
Arjunsinh Jadeja <arjunsinh@outlook.com>
2017-06-14 14:09:42 +0530

    test N read and delete first number

--- getNumber.php
+++ getNumber.php
@@ -3,9 +3,9 @@ echo "Base script working\n";
 $fp = fopen("requestQueue", "r+") or die("Unable to open file!");
 $queue = file_get_contents($fp);
 $first_num = substr($queue, 0, 9);
-echo $queue;
-echo $first_num;
-echo fgets($fp);
+echo "1:".$queue;
+echo "2:".$first_num;
+echo "3:".fgets($fp);
 file_put_contents($fp, substr($queue, 9 + 1));
 fclose($fp);
 ?>
