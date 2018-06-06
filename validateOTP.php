d763ea34a7c6f2535c850434e5e5f59a9e04d390
Arjunsinh Jadeja <mrarjunsinh@gmail.com>
2018-06-06 13:26:44 +0530

    Fix Validation

--- validateOTP.php
+++ validateOTP.php
@@ -2,25 +2,25 @@
 $CLIENT_KEY = "BR7234HT3471NM906";
 
 if($_GET[clientkey] === $CLIENT_KEY) {
-  echo "Hello World!";
   $num = $_GET["qnum"].PHP_EOL;
   $otp = $_GET["otp"].PHP_EOL;
 
-  $pos = getLineWithString($num, $otp);
+  $pos = getLineWithString("processedNumStack", $num);
 
   if($pos !== -1){
     //Found
-    $numbers = file(processedNumStack);
-    $otps = file(processedOTPStack);
+    $numbers = file("processedNumStack");
+    $otps = file("processedOTPStack");
 
-    if(($otp === $otps[$pos]) && ($num === $nnumbers[$pos])){
+    if(($otp === $otps[$pos]) && ($num === $numbers[$pos])){
       echo "True";
     } else {
       echo "False";
     }
+
   }
   else {
-    echo "False";
+    echo "False - Not found";
   }
 
 }
@@ -32,7 +32,7 @@ function getLineWithString($fileName, $str) {
     $lines = file($fileName);
     foreach ($lines as $lineNumber => $line) {
         if (strpos($line, $str) !== false) {
-            return $line;
+            return $lineNumber;
         }
     }
     return -1;
